fda02c102a29ad5e7b5513ba20c8d94ef31646ca
sujith.s <sujith.s@juegostudioz.com>
2021-10-28 16:27:10 +0530

    first commit tp main

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const serverless = require('serverless-http');
+require('dotenv').config();
+
+app.get('/', (req, res) => {
+  res.send('sucess github-lambda-deployment');
+})
+
+module.exports.handler = serverless(app)
+
+app.listen(process.env.PORT || 3000);
+console.log('app running on port : 3000')
